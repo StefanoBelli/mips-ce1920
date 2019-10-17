165c9132477a6452f12cd37cdf4ba9bb60a1cc7f
StefanoBelli <sa.belli@hotmail.it>
2019-10-17 19:27:39 +0200

    Added endianess.py

---
+++ endianess.py
@@ -0,0 +1,5 @@
+def big2little4(dw):
+    return ((dw & 0x000000FF) << 24) | ((dw & 0x0000FF00) << 16) | ((dw & 0x00FF0000) << 8) | ((dw & 0xFF000000) >> 24)
+
+def little2big4(dw):
+    return ((dw & 0xFF000000) >> 24) | ((dw & 0x00FF0000) >> 16) | ((dw & 0x0000FF00) >> 8) | ((dw & 0x000000FF) << 24)
