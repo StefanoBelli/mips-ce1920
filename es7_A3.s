b4770aa0fd57df060eb81615e7a6809e9138d2a2
StefanoBelli <sa.belli@hotmail.it>
2019-11-27 21:27:00 +0100

    es7_A3.s edited

--- es7_A3.s
+++ es7_A3.s
@@ -1,16 +1,18 @@
+# 0x0040D000 <-- determinare campi imm di beq, bne, j, jal
+
 .text
-min:
+min: # 0x40D000
 	slt $t0, $a0, $a1
-	bne $t0, $zero, __min_first # a0 < a1
+	bne $t0, $zero, __min_first			# __min_first = 2
 	add $t1, $zero, $a1
-	j __min_next
+	j __min_next						# 0x40D000 + (5 * 4)
 
 __min_first:
 	add $t1, $zero, $a0
 
 __min_next:
 	slt $t0, $t1, $a2
-	bne $t0, $zero, __min_second # t1 < a2
+	bne $t0, $zero, __min_second # t1 < a2 # __min_second = 2
 	add $v0, $zero, $a2
 	jr $ra
 
@@ -18,7 +20,7 @@ __min_second:
 	add $v0, $zero, $t1
 	jr $ra
 	
-g:
+g: # 0x40D044
 	addi $t0, $zero, 3
 	addi $v0, $zero, 4
 	slt $t1, $a0, $t0
