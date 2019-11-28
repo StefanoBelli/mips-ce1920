49aaf9490051d62920b1b631b6c7158b814b441f
Stefano Belli <sa.belli@hotmail.it>
2019-11-28 09:04:23 +0100

    tools.py

--- tools.py
+++ tools.py
@@ -3,3 +3,9 @@ def little2big4(dw):
 
 def big2little4(dw):
     return ((dw & 0xFF000000) >> 24) | ((dw & 0x00FF0000) >> 16) | ((dw & 0x0000FF00) >> 8) | ((dw & 0x000000FF) << 24)
+
+def j(pc, imm):
+    return (pc + 4) & 0xF0000000 | (imm << 2)
+
+def b(pc, imm):
+    return pc + 4 + (imm << 2)
